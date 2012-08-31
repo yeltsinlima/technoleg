66f81f3cfb049c75ab830db5b6170b529aa539a3
Yeltsin Lima <contato@dicaswp.com.br>
2012-08-31 20:09:19 -0300

    Inserção da Sidebar

--- index.php
+++ index.php
@@ -86,11 +86,204 @@
 		
 			<div class="row">
 			
+				<!-- Logo -->
 				<div class="span4">
 				
 					<h1><a href="index.html" title="Technoleg - Seu canal de Tecnologia, Games, Fotografia, Social Media">TECHNOLEG</a></h1>
 					<h2>Não, ainda não temos uma logo...</h2>
 				
+				</div>
+				
+				<!-- Pesquisa -->
+				<div id="pesquisa" class="span8">
+				
+					<div class="row">
+
+						<div class="span8">
+						
+							<form>
+							
+								<div class="row">
+								
+									<div class="offset2 span4">
+										<input type="text" name="s" id="s" placeholder="Digite a sua pesquisa" class="span4">
+									</div>
+								
+									<div class="span2">
+										<input type="submit" value="Pesquisar" class="btn btn-primary">
+									</div>
+							
+								
+									<div class="offset2 span6">
+									
+										<ul class="nav nav-pills">
+											<li class="active"><a href="index.php" title="Voltar para a página inicial">Home</a></li>
+											<li><a href="quemsomos.php" title="Saiba mais sobre a equipe do Technoleg">Quem Somos</a></li>
+											<li><a href="colabore.php" title="Envie dicas e artigos">Colabore</a></li>
+											<li><a href="anuncie.php" title="Anuncie Conosco">Anuncie</a></li>
+											<li><a href="trabalhe.php" title="Trabalhe Conosco">Vagas</a></li>
+										</ul>
+									
+									</div>
+									
+								</div>
+								
+							</form>
+						
+						</div>
+					
+					</div>
+				
+				</div>
+			
+			</div>
+		
+		</div>
+		
+		<div id="menu" class="container">
+		
+			<div class="row">
+			
+				<div class="span12">
+					
+					<div class="navbar">
+					
+						<div class="navbar-inner">
+						
+							<ul class="nav">
+								<li><a href="index.php" title="Voltar para a página inicial">Home</a></li>
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+										Eletrônicos
+										<b class="caret"></b>
+									</a>
+									<ul class="dropdown-menu">
+										<li><a href="http://www.technoleg.com.br/saiba-mais/acessorios/" title="Acessórios para Eletrônicos">Acessórios</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/armazenamento/" title="Armazenamento para Eletrônicos">Armazenamento</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/caixas-de-som/" title="Caixas de Som">Caixas de Som</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/componentes-para-computadores/" title="Componentes para Computadores">Componentes</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/placas/" title="Placas para Computadores">Placas</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/impressoras/" title="Impressoras">Impressoras</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/media-center/" title="Media Center">Media Center</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/monitores/" title="Monitores">Monitores</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/mp3-players/" title="MP3 Players">MP3 Players</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/notebooks/" title="Notebooks">Notebooks</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/televisoes/" title="Televisões">Televisões</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/roteadores/" title="Roteadores">Roteadores</a></li>
+										<li>
+									</ul>
+								</li>
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+										Mobile
+										<b class="caret"></b>
+									</a>
+									<ul class="dropdown-menu">
+										<li><a href="http://www.technoleg.com.br/saiba-mais/android/" title="Android">Android</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/iphone-ipad/" title="iPhone e iPad">iPhone e iPad</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/windows-phone/" title="Windows Phone 7">Windows Phone 7</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/symbian/" title="Symbian">Symbian</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/blackberry/" title="BlackBerry">BlackBerry</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/celulares/" title="Celulares">Celulares</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/tablets/" title="Tablets">Tablets</a></li>
+									</ul>
+								</li>
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+										Fotografia
+										<b class="caret"></b>
+									</a>
+									<ul class="dropdown-menu">
+										<li><a href="http://www.technoleg.com.br/saiba-mais/camera-digital-compacta/" title="Câmera Digital Compacta">Câmera Digital Compacta</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/camera-dslr/" title="Câmera DSLR">Câmera DSLR</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/tecnicas-fotografia/" title="Técnicas para Fotografia">Técnicas para Fotografia</a></li>
+									</ul>
+								</li>
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+										Internet
+										<b class="caret"></b>
+									</a>
+									<ul class="dropdown-menu">
+										<li><a href="http://www.technoleg.com.br/saiba-mais/facebook/" title="Facebook">Facebook</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/google/" title="Google">Google</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/msn/" title="MSN">MSN</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/orkut/" title="Orkut">Orkut</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/tumblr/" title="Tumblr">Tumblr</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/twitter/" title="Twitter">Twitter</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/wordpress/" title="WordPress">WordPress</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/empreendedorismo/" title="Empreendedorismo">Empreendedorismo</a></li>
+
+									</ul>
+								</li>
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+										Mobile
+										<b class="caret"></b>
+									</a>
+									<ul class="dropdown-menu">
+										<li><a href="http://www.technoleg.com.br/saiba-mais/jogos-mac/" title="Jogos para Mac">Mac</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/jogos-pc/" title="Jogos para PC">PC</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/jogos-xbox/" title="XBox 360">Xbox 360</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/jogos-ps3/" title="PlayStation 3">PlayStation 3</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/jogos-psvita/" title="PlayStation Vita">PlayStation Vita</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/jogos-3ds/" title="Nintendo 3DS">Nintendo 3DS</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/jogos-ds/" title="Nintendo DS">Nintendo DS</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/jogos-wii/" title="Nintendo Wii">Nintendo Wii</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/jogos-wii-u/" title="Nintendo Wii U">Nintendo Wii U</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/jogos-online/" title="Jogos Online">Jogos Online</a></li>
+										<li><a href="http://www.technoleg.com.br/saiba-mais/jogos-download/" title="Jogos para Download">Jogos para Download</a></li>
+									</ul>
+								</li>
+								<li><a href="http://www.technoleg.com.br/reviews/" title="Reviews">Reviews</a></li>
+								<li><a href="http://www.technoleg.com.br/podcast" title="Podcast">Technocast</a></li>
+								<li><a href="http://www.technoleg.com.br/contato" title="Contato">Contato</a></li>
+								<li><a href="http://www.technoleg.com.br/links" title="Links">Links</a></li>
+							</ul>
+						
+						</div>
+					
+					</div>
+				
+				</div>
+			
+			</div>
+		
+		</div>
+		
+		<div id="content" class="container">
+		
+			<div class="row">
+			
+				<div class="span8">
+	
+					teste
+					
+				</div>
+				
+				<div class="span4">
+					
+					<div class="row">
+					
+						<div class="widget span4">
+						
+							<img src="http://placehold.it/300x250">
+						
+						</div>
+						
+						<div class="widget span4">
+						
+							<div class="g-plus" data-href="//plus.google.com/107515263962257847874" data-rel="publisher"></div>
+
+							<div class="fb-like-box" data-href="http://www.facebook.com/technolegblog" data-width="300" data-show-faces="true" data-border-color="#CDCDCD" data-stream="false" data-header="false"></div>
+							
+							<a href="https://twitter.com/techno_leg" class="twitter-follow-button" data-show-count="true" data-lang="pt" data-size="large">Seguir @techno_leg</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+						
+						</div>
+					
+					</div>
+					
 				</div>
 			
 			</div>
@@ -98,6 +291,7 @@
 		</div>
 	
 	<!-- Javascript -->
+	<script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.0.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 	
 	<!-- Google Analyics -->
@@ -123,6 +317,23 @@
 		    });
 		});
 	</script>
+	
+	<script type="text/javascript">
+	  (function() {
+	    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+	    po.src = 'https://apis.google.com/js/plusone.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+	  })();
+	</script>
+	
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1&appId=180941515307561";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
 	
 	</body>
 
